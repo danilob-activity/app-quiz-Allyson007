ab4bca0cb3b84aa09da610690fb6c10b3b0981bf
Francisco Alysson <allydj2015@outlook.com>
2019-05-24 08:43:06 -0300

    Modifacôes no app quiz.

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -1,6 +1,8 @@
 package com.londonappbrewery.quizzler;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -55,8 +57,15 @@ public class MainActivity extends Activity {
         mQuestionTextView = findViewById(R.id.question_text_view);
         mProgressBar = findViewById(R.id.progress_bar);
 
-        mIndex = 0;
-        mScore = 0;
+        if (savedInstanceState != null) {
+            mScore = savedInstanceState.getInt("ScoreKey");
+            mIndex = savedInstanceState.getInt("IndexKey");
+        //atualiza texto do score
+        } else {
+            mScore = 0;
+            mIndex = 0;
+        }
+
         mQuestion = mQuestionBank[mIndex].getQuestionID();
         mQuestionTextView.setText(mQuestion);
         mScoreTextView.setText("Score" + mScore + "/" + mQuestionBank.length);
@@ -80,6 +89,8 @@ public class MainActivity extends Activity {
         });
     }
 
+    final int PROGRESS_BAR_INCREMENT = (int) Math.ceil(100.0 / mQuestionBank.length);
+
     public void checkAnswer(boolean answer) {
 
         if (answer == mQuestionBank[mIndex].isAnswer()) {
@@ -90,9 +101,49 @@ public class MainActivity extends Activity {
         }
     }
 
-    public void updateQuestion(){
+    private void updateQuestion(){
+
+        mScoreTextView.setText("Score: " + mScore + "/" + mQuestionBank.length);
+        mIndex++;
+        mIndex %= mQuestionBank.length;
+        mProgressBar.incrementProgressBy(PROGRESS_BAR_INCREMENT);
+
+        if(mIndex == 0){
+            AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+            alert.setTitle("Game Over");
+            alert.setCancelable(false);
+            alert.setMessage("You scored " + mScore + " points!");
+            alert.setNegativeButton("Restart Application", new
+                    DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            mScore = 0;
+                            mProgressBar.setProgress(0);
+                            mScoreTextView.setText("Score: " + mScore + "/" + mQuestionBank.length);
+                        }
+                    });
+            alert.setPositiveButton("Close Application", new
+                    DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    });
+            alert.show();
+        }
+
+        mQuestion = mQuestionBank[mIndex].getQuestionID();
+        mQuestionTextView.setText(mQuestion);
 
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("ScoreKey", mScore);
+        outState.putInt("IndexKey", mIndex);
+    }
+
 
     //public void ClickFalse(View view) {
         //Toast.makeText(getApplicationContext(), "True", Toast.LENGTH_SHORT).show();
